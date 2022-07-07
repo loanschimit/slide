e9b19bf42f8970227c8b21b28f13da70810e5588
Loan <loan.v.schimit1@gmail.com>
2022-07-07 00:44:03 -0300

    comentado

--- js/slide.js
+++ js/slide.js
@@ -3,40 +3,49 @@ export default class Slide {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
     this.dist = {
-      finalPosition: 0,
-      startX: 0,
-      movement: 0,
+      finalPosition: 0, // posição ao soltar o mouse
+      startX: 0, // posição onde mousedown ocorre
+      movement: 0, // startX - finalPosition
     };
   }
 
+  // bind(this) para puxar o this da class
   bindEvents() {
     this.onStart = this.onStart.bind(this);
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
   }
 
+  // previne o padrão, determina o startX como event.clientX, e adiciona o evento de mousemove
   onStart(event) {
     event.preventDefault();
     this.dist.startX = event.clientX;
-
     this.wrapper.addEventListener("mousemove", this.onMove);
   }
+
+  // move 'slide'
   moveSlide(distX) {
-    this.dist.movePosition = distX;
-    this.slide.style.transform = `translate3d(${distX}px, 0px, 0px)`;
+    this.dist.movePosition = distX; // = finalPosition
+    this.slide.style.transform = `translate3d(${distX}px, 0px, 0px)`; // move o slide no eixo X
   }
+
   updatePosition(clientX) {
-    this.dist.movement = (this.dist.startX - clientX) * 1.6;
-    return this.dist.finalPosition - this.dist.movement;
+    this.dist.movement = (this.dist.startX - clientX) * 1.6; // posição onde mousemove ocorre
+    return this.dist.finalPosition - this.dist.movement; // posição de transição no eixo X
   }
+
+  // determina que finalPosition será igual ao this.updatePosition(event.clientX) e executa moveSlide(finalPosition)
   onMove(event) {
     const finalPosition = this.updatePosition(event.clientX);
     this.moveSlide(finalPosition);
   }
+  
   onEnd(event) {
-    this.wrapper.removeEventListener("mousemove", this.onMove);
-    this.dist.finalPosition = this.dist.movePosition;
+    this.wrapper.removeEventListener("mousemove", this.onMove); // remove o evento mousemove de wrapper
+    this.dist.finalPosition = this.dist.movePosition; // determina que a posição final vai ser a do próximo inicio
   }
+
+  // cria os eventos de mousedown e mouseup
   addSlideEvents() {
     this.wrapper.addEventListener("mousedown", this.onStart);
     this.wrapper.addEventListener("mouseup", this.onEnd);
